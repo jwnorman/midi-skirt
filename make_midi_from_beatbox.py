a59c49bc2b3debbffad1bb3db8e3f367f822dbbc
admin <admin@admins-Mac-mini.attlocal.net>
2018-04-14 18:34:36 -0700

    refactoring wav to midi functions into easier to use class

--- make_midi_from_beatbox.py
+++ make_midi_from_beatbox.py
@@ -1,5 +1,6 @@
 # import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
 import peakutils
 # import seaborn as sns
@@ -10,110 +11,6 @@ from scipy.stats import rankdata
 from sklearn.cluster import KMeans
 
 from midi_skirt import *
-
-
-def load_wav_file(filename):
-    fs, data = wavfile.read(filename)
-    try:
-        channel0 = data[:,0]
-    except:
-        channel0 = data
-    return channel0
-
-
-def get_peak_info(data, thres=7000, min_dist=44100 / 4.0):
-    ids = peakutils.indexes(data, thres=thres, min_dist=min_dist)
-    peaks = data[ids]
-    filtered_ids_and_peaks = [(id_temp, peak) for (id_temp, peak) in zip(ids, peaks) if peak > thres]
-    ids = [temp[0] for temp in filtered_ids_and_peaks]
-    peaks = [temp[1] for temp in filtered_ids_and_peaks]
-    return (ids, peaks)
-
-
-def get_bin_markers(peak_ids, max_time_id):
-    bin_markers = []
-    bin_markers.append(0)
-    for index in range(len(peak_ids) - 1):
-        temp_mean = (peak_ids[index] + peak_ids[index + 1]) / 2
-        bin_markers.append(temp_mean)
-    bin_markers.append(max_time_id)
-    return bin_markers
-
-
-def get_lit(bins):
-    """
-    input: [30,40,80,120,180,300]
-    output: [(30,40), (40,80), (80,120), (120,180), (180,300)]
-    """
-    bins = zip(bins, np.roll(bins, -1))
-    bins.pop()
-    return bins
-
-
-def get_fft(segment):
-    fd = abs(np.fft.fft(segment))
-    freq = abs(np.fft.fftfreq(len(fd), 1 / float(44100)))
-    return fd, freq
-
-
-def get_prominent_freq(segment):
-    fd, freq = get_fft(segment)
-    smoothed = signal.savgol_filter(fd, 169, 3)
-    max_freq = np.argmax(smoothed)
-    freq_with_max_amplitude = freq[max_freq]
-    return freq_with_max_amplitude
-
-
-def create_signatures(segments):
-    promiment_freqs = [get_prominent_freq(segment) for segment in segments]
-    return pd.DataFrame({
-        "prominent_freq": promiment_freqs
-    })
-
-
-def cluster_signatures(signatures, k=2):
-    kmeans = KMeans(n_clusters=k, random_state=0, n_init=100).fit(signatures)
-#    rank_mapping = dict(zip(kmeans.labels_, rankdata(kmeans.cluster_centers_, "dense")))
-    rank_mapping = dict(zip(range(k), rankdata(kmeans.cluster_centers_, "dense")))
-    print kmeans.labels_
-    print kmeans.cluster_centers_
-    import pdb; pdb.set_trace()
-    return pd.DataFrame({
-        "clusters": kmeans.labels_,
-        "rank": [rank_mapping[label] for label in kmeans.labels_]
-    })
-
-
-# signatures = pd.DataFrame([340, 5000, 250, 4000, 290, 4600, 468, 2668], columns=["prominent_freq"])
-signatures = pd.DataFrame([259, 4400, 280, 5400, 5300, 5499, 1702], columns=["prominent_freq"])
-
-def get_midi_info(time_ids, peaks, signatures, clusters):
-    info = pd.DataFrame({
-        "time_ids": time_ids,
-        "peaks": peaks
-    })
-    return pd.concat([info, clusters, signatures], axis=1)
-
-
-def get_midi_info_from_wav(filename):
-    data = load_wav_file(filename)
-    time_ids, peaks = get_peak_info(data, 6000, 44100 / 8.0)
-    bin_markers = get_bin_markers(time_ids, len(data))
-    buckets = get_lit(bin_markers)
-    segments = [data[np.arange(int(bucket[0]), int(bucket[1]))] for bucket in buckets]
-    signatures = create_signatures(segments)
-    clusters = cluster_signatures(signatures, 2)
-    midi_info = get_midi_info(time_ids, peaks, signatures, clusters)
-    return midi_info
-
-
-
-
-# map from cluster to drum
-cluster_to_drum_mapping = {
-    1: "C3",
-    2: "D3"
-}
 
 
 class DrumMapping:
@@ -133,45 +30,146 @@ class DrumMapping:
         pass
 
 
-def convert_amplitude_to_velocity(amp, min_amp, max_amp, min_vel, max_vel):
-    try:
-        return int((max_vel - min_vel) / float(max_amp - min_amp) * amp + min_vel)
-    except:
-        return 0
+class WavToMidi:
+    def __init__(self, filename, rate, drum_mapping):
+        self.filename = filename
+        self.rate = rate
+        self.pc = PatternConstants(resolution=440)
+        self.pattern = midi.Pattern(resolution=self.pc.resolution)
+        self.track = midi.Track()
+        self.pattern.append(self.track)
+        self.all_staged_events = []
+        self.drum_mapping = drum_mapping
 
+    # @staticmethod
+    def _load_wav_file(self):
+        print "Loading wav file"
+        fs, data = wavfile.read(self.filename)
+        try:
+            channel0 = data[:,0]
+        except:
+            channel0 = data
+        return channel0
 
-def convert_sample_to_tick(pc, time_id, num_samples=44000):
-    spb = 60 / 120.0
-    tpb = pc.resolution
-    return int((time_id / float(num_samples)) * (1.0 / spb * tpb))
+    # @staticmethod
+    def _get_peak_info(self, data, thres, min_dist):
+        print "Finding peaks"
+        ids = peakutils.indexes(data, thres=thres, min_dist=min_dist)
+        peaks = data[ids]
+        filtered_ids_and_peaks = [(id_temp, peak) for (id_temp, peak) in zip(ids, peaks) if peak > thres]
+        ids = [temp[0] for temp in filtered_ids_and_peaks]
+        peaks = [temp[1] for temp in filtered_ids_and_peaks]
+        return (ids, peaks)
 
+    # @staticmethod
+    def _get_bin_markers(self, peak_ids, max_time_id):
+        print "Finding bin markers"
+        bin_markers = []
+        bin_markers.append(0)
+        for index in range(len(peak_ids) - 1):
+            temp_mean = (peak_ids[index] + peak_ids[index + 1]) / 2
+            bin_markers.append(temp_mean)
+        bin_markers.append(max_time_id)
+        return bin_markers
 
-def convert_wav_to_midi(filename):
-    mi = get_midi_info_from_wav(filename)
+    # @staticmethod
+    def _get_lit(self, bins):
+        """
+        input: [30,40,80,120,180,300]
+        output: [(30,40), (40,80), (80,120), (120,180), (180,300)]
+        """
+        bins = zip(bins, np.roll(bins, -1))
+        bins.pop()
+        return bins
 
-    pc = PatternConstants(resolution=440)
-    pattern = midi.Pattern(resolution=pc.resolution)
-    track = midi.Track()
-    pattern.append(track)
-    all_staged_events = []
-    min_amp = mi.peaks.min()
-    max_amp = mi.peaks.max()
-    import pdb; pdb.set_trace()
-    drum_mapping = DrumMapping().kick_and_snare
-    for row in mi.itertuples():
-        # drum = cluster_to_drum_mapping[row.clusters]
-        drum = drum_mapping(row.rank)
-        vel = convert_amplitude_to_velocity(row.peaks, min_amp, max_amp, 50, 100)
-        tick = convert_sample_to_tick(pc, row.time_ids)
-        all_staged_events.append(MidiEventStager(midi.NoteOnEvent, tick, pc.eighth_note, drum, vel))
-        all_staged_events.append(MidiEventStager(midi.NoteOffEvent, tick + pc.eighth_note, pc.eighth_note, drum, vel))
-    df = convert_staging_events_to_dataframe(all_staged_events)
-    df.sort_values(by=["tick", "duration"], inplace=True)
-    track = add_tuples_to_track(track, df)
-    eot = midi.EndOfTrackEvent(tick=get_max_tick(track) + 2 * pc.whole_note)
-    track.append(eot)
-    track = make_ticks_rel(track)
-    return pattern
+    # @staticmethod
+    def _get_fft(self, segment, rate):
+        print "Applying FFT"
+        fd = abs(np.fft.fft(segment))
+        freq = abs(np.fft.fftfreq(len(fd), 1 / float(rate)))
+        print "Done with FFT"
+        return fd, freq
+
+    # @staticmethod
+    def _get_prominent_freq(self, segment, rate):
+        print "Finding prominent frequencies"
+        fd, freq = self._get_fft(segment, rate)
+        smoothed = signal.savgol_filter(fd, 169, 3)
+        max_freq = np.argmax(smoothed)
+        freq_with_max_amplitude = freq[max_freq]
+        return freq_with_max_amplitude
+
+    # @staticmethod
+    def _create_signatures(self, segments):
+        print "Generating sound signatures"
+        promiment_freqs = [self._get_prominent_freq(segment, self.rate) for segment in segments]
+        return pd.DataFrame({
+            "prominent_freq": promiment_freqs
+        })
+
+    # @staticmethod
+    def _cluster_signatures(self, signatures, k=2):
+        print "Clustering signatures"
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init=100).fit(signatures)
+        rank_mapping = dict(zip(range(k), rankdata(kmeans.cluster_centers_, "dense")))
+        return pd.DataFrame({
+            "clusters": kmeans.labels_,
+            "rank": [rank_mapping[label] for label in kmeans.labels_]
+        })
+
+    # @staticmethod
+    def _get_midi_info(self, time_ids, peaks, signatures, clusters):
+        info = pd.DataFrame({
+            "time_ids": time_ids,
+            "peaks": peaks
+        })
+        return pd.concat([info, clusters, signatures], axis=1)
+
+    def _get_midi_info_from_wav(self):
+        data = self._load_wav_file()
+        time_ids, peaks = self._get_peak_info(data, 6000, self.rate / 8.0)
+        bin_markers = self._get_bin_markers(time_ids, len(data))
+        buckets = self._get_lit(bin_markers)
+        segments = [data[np.arange(int(bucket[0]), int(bucket[1]))] for bucket in buckets]
+        signatures = self._create_signatures(segments)
+        clusters = self._cluster_signatures(signatures, 2)
+        midi_info = self._get_midi_info(time_ids, peaks, signatures, clusters)
+        return midi_info
+
+    def convert_amplitude_to_velocity(self, amp, min_amp, max_amp, min_vel, max_vel):
+        try:
+            return int((max_vel - min_vel) / float(max_amp - min_amp) * amp + min_vel)
+        except:
+            return 0
+
+    def convert_sample_to_tick(self, pc, time_id, num_samples=44100):
+        spb = 60 / 120.0
+        tpb = pc.resolution
+        return int((time_id / float(num_samples)) * (1.0 / spb * tpb))
+
+    def convert_wav_to_midi(self):
+        mi = self._get_midi_info_from_wav()
+        # pc = PatternConstants(resolution=440)
+        # pattern = midi.Pattern(resolution=pc.resolution)
+        # track = midi.Track()
+        # pattern.append(track)
+        # all_staged_events = []
+        min_amp = mi.peaks.min()
+        max_amp = mi.peaks.max()
+        # drum_mapping = DrumMapping().kick_and_snare
+        for row in mi.itertuples():
+            drum = self.drum_mapping(row.rank)
+            vel = self.convert_amplitude_to_velocity(row.peaks, min_amp, max_amp, 50, 100)
+            tick = self.convert_sample_to_tick(self.pc, row.time_ids)
+            self.all_staged_events.append(MidiEventStager(midi.NoteOnEvent, tick, self.pc.eighth_note, drum, vel))
+            self.all_staged_events.append(MidiEventStager(midi.NoteOffEvent, tick + self.pc.eighth_note, self.pc.eighth_note, drum, vel))
+        df = convert_staging_events_to_dataframe(self.all_staged_events)
+        df.sort_values(by=["tick", "duration"], inplace=True)
+        self.track = add_tuples_to_track(self.track, df)
+        eot = midi.EndOfTrackEvent(tick=get_max_tick(self.track) + 2 * self.pc.whole_note)
+        self.track.append(eot)
+        self.track = make_ticks_rel(self.track)
+        return self.pattern
 
 # os.system("sox /Users/jacknorman1/Desktop/beatbox_2.wav /Users/jacknorman1/Desktop/beatbox_2.wav")
 
@@ -184,6 +182,27 @@ filename = "/Users/jacknorman1/Desktop/beatbox_2_copy.wav"
 pattern = convert_wav_to_midi(filename)
 midi.write_midifile("example.mid", pattern)
 
+
+filename = "/Users/admin/Desktop/BeatBoxExamples/sample2_copy.wav"
+rate = 44100/4
+os.system("sox {} --rate {} temp_file.wav".format(filename, rate))
+pattern = convert_wav_to_midi("temp_file.wav", rate)
+midi.write_midifile("example.mid", pattern)
+os.system("rm temp_file.wav")
+drum_mapping = DrumMapping().kick_and_snare
+
+
+
+
+
+filename = "/Users/admin/Desktop/BeatBoxExamples/sample3.wav"
+rate = 44100
+drum_mapping = DrumMapping().kick_and_snare
+os.system("sox {} --rate {} temp_file.wav".format(filename, rate))
+wav_to_midi = WavToMidi("temp_file.wav", rate, drum_mapping)
+pattern = wav_to_midi.convert_wav_to_midi()
+midi.write_midifile("example.mid", pattern)
+os.system("rm temp_file.wav")
 
 
 
